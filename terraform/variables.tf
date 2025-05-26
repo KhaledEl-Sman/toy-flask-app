c927211f1846521f6e56d61859f6d607a696ff0c
KhaledEl-Sman <k.elsman1997@gmail.com>
2025-05-26 18:05:15 +0000

    update variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,8 +1,7 @@
-variable "project_name_prefix" {}
-variable "domain_name" {}
-variable "vpc_cidr" {}
-variable "subnet_az" {}
-variable "subnet_cidr" {}
-variable "instance_type" {}
-variable "ssh_cidr_block" {}
-variable "ssh_file_location" {}
+variable "project_name_prefix" {default = "botit"}
+variable "domain_name" {default = "zevar.me"}
+variable "vpc_cidr" {default = "10.0.0.0/16"}
+variable "subnet_az" {default = "eu-west-1c"}
+variable "subnet_cidr" {default = "10.0.0.0/20"}
+variable "instance_type" {default = "t3.micro"}
+variable "ssh_cidr_block" {default = "78.47.221.219/32"}
