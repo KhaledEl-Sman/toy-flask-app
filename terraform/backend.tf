02879b894e72d4174cd6e1b87762cc538ee32e97
KhaledEl-Sman <k.elsman1997@gmail.com>
2025-05-26 19:09:13 +0000

    add backend on s3 & dynamodb

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,11 @@
 terraform {
   backend "s3" {
-    bucket         = "${var.project_name_prefix}-terraform-state"
+    bucket         = "botit-terraform-state"
     key            = "envs/prod/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-locks"
     encrypt        = true
   }
 }
+
+
