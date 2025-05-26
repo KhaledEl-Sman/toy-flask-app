e85b406283d99131129ea63b4036aaaaa8e0a98c
KhaledEl-Sman <k.elsman1997@gmail.com>
2025-05-26 19:31:44 +0000

    backend setup

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "botit-terraform-state"
+    bucket         = "botit-terraform-state-eu"
     key            = "envs/prod/terraform.tfstate"
     region         = "eu-west-1"
     dynamodb_table = "terraform-locks"
